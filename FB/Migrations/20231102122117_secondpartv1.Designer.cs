d8cc5d782a50ee37a9e40768a363bd8cbfb1b5e4
jcribaya <jaycarlos29@gmail.com>
2023-11-02 20:22:27 +0800

    New Feature: Add Blog

--- FB/Migrations/20231102122117_secondpartv1.Designer.cs
+++ FB/Migrations/20231102122117_secondpartv1.Designer.cs
@@ -4,6 +4,7 @@ using FB.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace FB.Migrations
 {
     [DbContext(typeof(FB_DBContext))]
-    partial class FB_DBContextModelSnapshot : ModelSnapshot
+    [Migration("20231102122117_secondpartv1")]
+    partial class secondpartv1
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ FB/Migrations/20231102122117_secondpartv1.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FB.Migrations
+{
+    /// <inheritdoc />
+    public partial class secondpartv1 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "PulishDate",
+                table: "BlogPosts",
+                newName: "PublishedDate");
+
+            migrationBuilder.RenameColumn(
+                name: "FeaturedTitle",
+                table: "BlogPosts",
+                newName: "FeaturedImageUrl");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "PublishedDate",
+                table: "BlogPosts",
+                newName: "PulishDate");
+
+            migrationBuilder.RenameColumn(
+                name: "FeaturedImageUrl",
+                table: "BlogPosts",
+                newName: "FeaturedTitle");
+        }
+    }
+}
